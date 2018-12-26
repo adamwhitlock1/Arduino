9eca076ee8079e2fd94ef85605b794ba1480d980
Adam Whitlock <adam.whitlock627@gmail.com>
2018-12-26 07:15:21 -0700

    added incremental led brightness sketch

--- sketch_dec25a/sketch_dec25a.ino
+++ sketch_dec25a/sketch_dec25a.ino
@@ -1,9 +1,20 @@
+
+// blink led
+
+int ledPin = 13;
+
 void setup() {
-  // put your setup code here, to run once:
+
+//  initialize the pins as outputs
+pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+digitalWrite(ledPin, HIGH);
+delay(1500);
+digitalWrite(ledPin, LOW);
+delay(1500);
+  
+}`
